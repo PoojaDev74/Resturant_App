c9464cbe91f60d77585682f45e548a5ea29a86f1
Pooja D <poojapreethi2001@gmail.com>
2025-10-24 11:37:27 +0530

    Fix import statement for restaurant logo

--- user/src/components/SideBar/SideBar.jsx
+++ user/src/components/SideBar/SideBar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./SideBar.css";
 import { NavLink } from "react-router-dom";
-import resturant_logo from "../../assests/assets";
+import { resturant_logo } from "../../assests/assets";
 const SideBar = () => {
   return (
     <header id="header" className="header">
